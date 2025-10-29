e4dd4a723acdec1fa36654c7c0d869f765d00b33
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-10-29 10:02:09 +0100

    timestamp added to Order record and instantiations changed accordingly, error fixed

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,11 +1,23 @@
 import lombok.With;
 
+import java.time.Instant;
 import java.util.List;
 
 @With
 public record Order(
         String id,
         List<Product> products,
-        OrderStatus orderStatus
+        OrderStatus orderStatus,
+        Instant timeOfOrder
 ) {
+    public Order(String id, List<Product> products, OrderStatus orderStatus, Instant timeOfOrder) {
+        this.id = id;
+        this.products = products;
+        this.orderStatus = orderStatus;
+        this.timeOfOrder = timeOfOrder;
+    }
+
+    public Order(String id, List<Product> products, OrderStatus orderStatus) {
+        this(id, products, orderStatus, null);
+    }
 }
